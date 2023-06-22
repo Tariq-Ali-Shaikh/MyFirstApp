4326ef1c45627ec1b8cd1d1f3c10976fb5b35ba0
Tas Pro <t304pkdummy@hotmail.com>
2023-06-22 10:41:52 +0500

    aoljaljfd

--- Form1.vb
+++ Form1.vb
@@ -2,7 +2,7 @@
 
 
     Private Sub ClickButton_Click(sender As Object, e As EventArgs) Handles ClickButton.Click
-        GreetingLabel.Text = "Hello, " + NameText.Text + "!"
+        GreetingLabel.Text = "Hello, " + NameText.Text
     End Sub
 
 End Class
